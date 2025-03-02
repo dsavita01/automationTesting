2f2a036d9dc22f09688e14017ed687ae2c2af17f
unknown <deepaksavita014@gmail.com>
2025-03-02 15:57:10 +0530

    intial commit

---
+++ src/test/java/GitLearning/GitLearning/DateTime.java
@@ -0,0 +1,14 @@
+package GitLearning.GitLearning;
+
+import java.util.Date;
+
+public class DateTime {
+
+	public static void main(String[] args) {
+		
+		Date current_Date = new Date();
+        System.out.println(current_Date);
+
+	}
+
+}
